61b89c194f5a7765146d7d9c202a0ab46462bee6
EnneS <nathan.soulier26@gmail.com>
2018-02-07 03:09:13 +0100

    oui

--- window.rb
+++ window.rb
@@ -5,16 +5,20 @@ class Window < Gosu::Window
   def initialize(width, height)
     super
     self.caption = "Hardcore Survival"
-    @map = Map.new()
-    @map.generate(8, 3000, 40, 8, 7, 64)
-    #@map.load()
-    @hero = Hero.new(100, 100, @map)
-    @song = Gosu::Song.new("res/music.mp3")
-    @song.volume = 0.25
-    @song.play(true)
 
     @background_image = Gosu::Image.new("res/blue.jpg")
 
+    @map = Map.new()
+    #@map.generate(10, 150, 23, 7, 6, 32)
+    @map.load()
+
+    @hero = Hero.new(100, 100, @map)
+    @inventaire = Inventaire.new(10)
+    @inventaire.store(1, 4)
+    @inventaire.store(2, 7)
+
+
+    @cursor = Gosu::Image.new("res/cursor.png")
     @camera_x = @camera_y = 0
 
   end
@@ -22,11 +26,12 @@ class Window < Gosu::Window
   def update
     # Actions du héro
     move_x = 0
-    move_x -= 10 if Gosu.button_down? Gosu::KB_LEFT
-    move_x += 10 if Gosu.button_down? Gosu::KB_RIGHT
+    move_x -= 6 if Gosu.button_down? Gosu::KB_LEFT
+    move_x += 6 if Gosu.button_down? Gosu::KB_RIGHT
     @hero.update(move_x)
     @hero.jump if Gosu::button_down?(Gosu::KbSpace)
-    @hero.sprint if Gosu::button_down?(Gosu::KbLeftShift)
+    @hero.setSprinting(true) if Gosu::button_down?(Gosu::KbLeftShift)
+    @hero.setSprinting(false) if !Gosu::button_down?(Gosu::KbLeftShift)
 
     @camera_x = [[@hero.x - WIDTH / 2, 0].max, (1280*30) * 50 - WIDTH].min
     @camera_y = [[@hero.y - HEIGHT / 2, 0].max, 150*30 * 50 - HEIGHT].min
@@ -36,7 +41,8 @@ class Window < Gosu::Window
 
   def draw
     @background_image.draw 0, 0, -2
-
+    @cursor.draw self.mouse_x, self.mouse_y, 99
+    @inventaire.draw
     Gosu.translate(-@camera_x, -@camera_y) do
       @hero.draw
       @map.draw(@hero.x, @hero.y)
