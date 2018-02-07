243cdfca2720c3d5e9e902b52bdf660d8ff22751
JUAN TAKO <juan.tako.dev@gmail.com>
2018-02-07 14:48:19 +0100

    resolved conflicts

--- window.rb
+++ window.rb
@@ -11,7 +11,7 @@ class Window < Gosu::Window
     @background_image = Gosu::Image.new("res/blue.jpg")
 
     @map = Map.new()
-    @map.generate(3, 3000, 40, 8, 7, 128)
+    @map.generate(3, 3000, 128, 8, 7, 60)
     #@map.load()
     @hero = Hero.new(((@map.data.size-1)*60)/2, 250, @map)
     @inventaire = Inventaire.new(10)
